7d763feb630452c087514d14b98a15b38bc20fb4
Arthur Cáceres Melo <arthurcaceres@gmail.com>
2022-09-23 22:16:51 -0300

    Adding get and post methods

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -220,21 +220,22 @@
 
             <div class="informacao-pagina">
                 <div class="contato-principal">
-                    <form>
-                        <input type="text" placeholder="Nome *" class="borda-preta" required>
+                    <form action="{{ route('site.contato') }}" method="post">
+                        @csrf
+                        <input name="nome" type="text" placeholder="Nome *" class="borda-preta" required>
                         <br>
-                        <input type="text" placeholder="Telefone *" class="borda-preta" required>
+                        <input name="telefone" type="text" placeholder="Telefone *" class="borda-preta" required>
                         <br>
-                        <input type="text" placeholder="E-mail *" class="borda-preta" required>
+                        <input name="email" type="text" placeholder="E-mail *" class="borda-preta" required>
                         <br>
                         <select class="borda-preta" required>
                             <option value="" selected>Qual o motivo do contato? *</option>
-                            <option value="">Dúvida</option>
-                            <option value="">Elogio</option>
-                            <option value="">Reclamação</option>
+                            <option value="1">Dúvida</option>
+                            <option value="2">Elogio</option>
+                            <option value="3">Reclamação</option>
                         </select>
                         <br>
-                        <textarea class="borda-preta" placeholder="Preencha aqui a sua mensagem"></textarea>
+                        <textarea class="borda-preta" name="mensagem" placeholder="Preencha aqui a sua mensagem"></textarea>
                         <br>
                         <button type="submit" class="borda-preta">ENVIAR</button>
                     </form>
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [\App\Http\Controllers\PrincipalController::class,'index'])->name('site.index');
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class,'sobreNos'])->name('site.sobrenos');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
+Route::post('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
 
 Route::prefix('app')->group(function () {
     Route::get('/clientes', function () { return 'Clientes.<br><br><a href="'.route('site.index').'">Home</a>'; })->name('app.cliente');
