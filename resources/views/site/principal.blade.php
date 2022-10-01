2b0d063308a02350e233dfc063890c851a3ecd85
Arthur Cáceres Melo <arthurcaceres@gmail.com>
2022-10-01 15:22:30 -0300

    Creating models and migrations

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -167,13 +167,12 @@
 
             <div class="direita">
                 <div class="contato">
-                    <h1>Contato</h1>
-                    <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
                     @component('layouts._components.formContato')
+                        <h1>Contato</h1>
+                        <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
                     @endcomponent
                 </div>
             </div>
-
             @component('layouts._components.footer')
             @endcomponent
         </div>
